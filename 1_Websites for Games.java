9b54104e88c0b624f2adf0a75d3558457bca3770
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-08 14:40:52 +0300

    Create 1_Websites for Games.java

---
+++ 1_Websites for Games.java
@@ -0,0 +1,30 @@
+There are several websites where you can download complete games as executables (EXE files) that can be played offline on your PC without needing additional downloads. Here are a few examples:
+
+1. **Steam** (https://store.steampowered.com/):
+   - Steam offers a vast collection of games that can be downloaded and played offline after installation. Many of these games come in the form of EXE installers that you can run to install the game on your PC.
+
+2. **GOG.com** (https://www.gog.com/):
+   - GOG.com (Good Old Games) specializes in offering DRM-free games that can be downloaded and played offline. They provide a wide selection of classic and modern games in EXE format.
+
+3. **itch.io** (https://itch.io/):
+   - itch.io hosts a diverse range of indie games, many of which are available for download as standalone EXE files. You can find a variety of free and paid games on itch.io that can be played offline.
+
+4. **Game Jolt** (https://gamejolt.com/):
+   - Game Jolt is another platform that hosts indie games, including many that can be downloaded and played offline. They offer a mix of free and commercial games in EXE format.
+
+5. **IndieDB** (https://www.indiedb.com/):
+   - IndieDB is a community-driven platform for indie game developers. You can find a variety of indie games available for download as standalone EXE files, suitable for offline play.
+
+Examples of games that you can download and play offline from these platforms include:
+
+- Stardew Valley
+- Hollow Knight
+- Cuphead
+- Terraria
+- Celeste
+- Undertale
+- The Witcher series
+- Civilization series
+- Age of Empires series
+
+These are just a few examples, and there are many more games available on these platforms that you can download and play offline on your PC.
